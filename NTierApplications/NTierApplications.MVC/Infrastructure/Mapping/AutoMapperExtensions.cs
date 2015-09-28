55c60a3ee51a67c7a7d18b90c03b551bdfee476f
burtonsmith <karatekid00seven@gmail.com>
2015-09-28 16:54:18 -0500

    Added automapper extension for ignoring attributes.

---
+++ NTierApplications/NTierApplications.MVC/Infrastructure/Mapping/AutoMapperExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using AutoMapper;
+
+namespace NTierApplications.MVC.Infrastructure.Mapping
+{
+	public static class AutoMapperExtensions
+	{
+		public static IMappingExpression<TSource, TDestination> IgnoreUnmappedAttributes<TSource, TDestination>(this IMappingExpression<TSource, TDestination> expression)
+		{
+			var sourceType = typeof(TSource);
+			var destinationType = typeof(TDestination);
+			var existingMaps = Mapper.GetAllTypeMaps().First(x => x.SourceType == sourceType && x.DestinationType == destinationType);
+			foreach (var property in existingMaps.GetUnmappedPropertyNames())
+			{
+				expression.ForMember(property, opt => opt.Ignore());
+			}
+			return expression;
+			
+			//  EXAMPLE:
+			//  Mapper.CreateMap<SourceType, DestinationType>()
+			//  	.ForMember(prop => x.Property, opt => opt.MapFrom(src => src.OtherProperty))
+			//  	.IgnoreUnmappedAttributes();
+		}
+	}
+}
