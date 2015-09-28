efb8041e5ab85e4c1beeb829556d466c238c2a10
burtonsmith <karatekid00seven@gmail.com>
2015-09-28 16:50:18 -0500

    Added the Queryable interface.

--- NTierApplications/NTierApplications.Domain/ServiceInterfaces/IUserRoleService.cs
+++ NTierApplications/NTierApplications.Domain/ServiceInterfaces/IUserRoleService.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNet.Identity;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
 using NTierApplications.Domain.Entities;
 
 namespace NTierApplications.Domain.ServiceInterfaces
 {
-	public interface IUserRoleService : IRoleStore<UserRole, int>
+	public interface IUserRoleService : IRoleStore<UserRole, int>, IQueryableRoleStore<UserRole, int>
 	{
-		 
-	}
+
+    }
 }
--- NTierApplications/NTierApplications.Domain/Services/UserRoleService.cs
+++ NTierApplications/NTierApplications.Domain/Services/UserRoleService.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using Microsoft.Win32.SafeHandles;
 using NTierApplications.Domain.Entities;
 using NTierApplications.Domain.Repositories;
 using NTierApplications.Domain.ServiceInterfaces;
@@ -10,6 +13,8 @@ namespace NTierApplications.Domain.Services
 	public class UserRoleService : IUserRoleService
 	{
 		private readonly IRepository<UserRole> _userRoleRepository;
+		bool _disposed;
+		readonly SafeHandle _handle = new SafeFileHandle(IntPtr.Zero, true);
 
 		public UserRoleService(IRepository<UserRole> userRoleRepository)
 		{
@@ -24,23 +29,38 @@ namespace NTierApplications.Domain.Services
 			await Task.Run(() => _userRoleRepository.Insert(role));
 		}
 
+		/// <summary>
+		///     Use DeleteByIdAsync instead as this it will throw an exception when the new instance of the object is created in the Repository.
+		/// </summary>
 		public async Task DeleteAsync(UserRole role)
 		{
-			if(role == null)
-				throw new ArgumentNullException("role");
+            if (role == null)
+                throw new ArgumentNullException("role");
 
-			await Task.Run((() => _userRoleRepository.Delete(role.Id)));
+            await Task.Run((() => _userRoleRepository.Delete(role.Id)));
+        }
+
+        public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
 		}
 
-		public void Dispose()
+		protected virtual void Dispose(bool disposing)
 		{
-			throw new NotImplementedException();
+			if (_disposed)
+				return;
+
+			if (disposing)
+				_handle.Dispose();
+
+			_disposed = true;
 		}
 
 		public async Task<UserRole> FindByIdAsync(int roleId)
 		{
 			if(roleId == 0)
-				throw new ArgumentNullException("roleId");
+				throw new ArgumentNullException("roleId", "You must add a roleId.");
 
 			return await Task.FromResult(_userRoleRepository.GetById(roleId));
 		}
@@ -53,12 +73,30 @@ namespace NTierApplications.Domain.Services
 			return await Task.FromResult(_userRoleRepository.Table.FirstOrDefault(x => x.Name == roleName));
 		}
 
+		public IEnumerable<UserRole> GetAll()
+		{
+			return _userRoleRepository.Table;
+		}
+
 		public async Task UpdateAsync(UserRole role)
 		{
 			if(role == null)
 				throw new ArgumentNullException("role");
 
 			await Task.Run((() => _userRoleRepository.Update(role)));
+		}
+
+		public IQueryable<UserRole> Roles
+		{
+			get { return _userRoleRepository.Table.AsQueryable(); }
+		}
+
+		public async Task DeleteByIdAsync(int id)
+		{
+			if (id == default(int))
+				throw new ArgumentNullException("id");
+
+			await Task.Run((() => _userRoleRepository.Delete(id)));
 		}
 	}
 }
