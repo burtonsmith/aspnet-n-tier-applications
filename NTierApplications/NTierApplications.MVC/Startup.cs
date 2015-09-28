a44add67713e48520e019407554f2c4e4f764f8b
burtonsmith <karatekid00seven@gmail.com>
2015-09-28 16:55:36 -0500

    Required for Owin.

---
+++ NTierApplications/NTierApplications.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(NTierApplications.MVC.Startup))]
+namespace NTierApplications.MVC
+{
+	public partial class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			ConfigureAuth(app);
+		}
+	}
+}
